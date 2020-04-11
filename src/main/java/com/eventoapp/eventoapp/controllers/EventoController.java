63c5e6a701b6f4c927c1a209ade389722ed6bca5
Carlos Henrique <ch.ferreir@hotmail.com>
2020-04-11 14:11:45 -0300

    Detalhes do Evento

--- src/main/java/com/eventoapp/eventoapp/controllers/EventoController.java
+++ src/main/java/com/eventoapp/eventoapp/controllers/EventoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -34,6 +35,15 @@ public class EventoController {
 		ModelAndView mv = new ModelAndView("index");
 		List<Evento> eventos = eventoRepository.findAll();
 		mv.addObject("eventos", eventos);
+		
+		return mv;
+	}
+	
+	@RequestMapping("/{codigo}")
+	public ModelAndView detalhesEvento(@PathVariable long codigo) {
+		Evento evento = eventoRepository.findByCodigo(codigo);
+		ModelAndView mv = new ModelAndView("evento/detalhesEvento");
+		mv.addObject("evento", evento);
 		
 		return mv;
 	}
--- src/main/java/com/eventoapp/eventoapp/repository/EventoRepository.java
+++ src/main/java/com/eventoapp/eventoapp/repository/EventoRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.repository.CrudRepository;
 import com.eventoapp.eventoapp.models.Evento;
 
 public interface EventoRepository extends JpaRepository<Evento, String> {
-
+	
+	Evento findByCodigo(long codigo);
 	
 }
