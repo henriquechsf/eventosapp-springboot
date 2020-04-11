694bce4bd2dcec6b7dd01b70665c1e9abfaa7df7
Carlos Henrique <ch.ferreir@hotmail.com>
2020-04-11 12:30:59 -0300

    crud Mysql com Spring Boot

---
+++ src/main/java/com/eventoapp/eventoapp/repository/EventoRepository.java
@@ -0,0 +1,11 @@
+package com.eventoapp.eventoapp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+
+import com.eventoapp.eventoapp.models.Evento;
+
+public interface EventoRepository extends JpaRepository<Evento, String> {
+
+	
+}
