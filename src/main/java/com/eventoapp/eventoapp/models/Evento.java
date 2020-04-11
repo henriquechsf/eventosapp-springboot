d928aa44d16402aba429c2b1df06c0c75dc2e370
Carlos Henrique <ch.ferreir@hotmail.com>
2020-04-11 14:29:51 -0300

    Formulário em Detalhes do Evento

--- src/main/java/com/eventoapp/eventoapp/models/Evento.java
+++ src/main/java/com/eventoapp/eventoapp/models/Evento.java
@@ -1,11 +1,13 @@
 package com.eventoapp.eventoapp.models;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Evento implements Serializable {
@@ -20,6 +22,10 @@ public class Evento implements Serializable {
 	private String local;
 	private String data;
 	private String horario;
+
+	// relacionamento entre entidades
+	@OneToMany
+	private List<Convidado> convidados;
 
 	public String getNome() {
 		return nome;
@@ -59,6 +65,14 @@ public class Evento implements Serializable {
 
 	public void setCodigo(long codigo) {
 		this.codigo = codigo;
+	}
+
+	public List<Convidado> getConvidados() {
+		return convidados;
+	}
+
+	public void setConvidados(List<Convidado> convidados) {
+		this.convidados = convidados;
 	}
 
 }
